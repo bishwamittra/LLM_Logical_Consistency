1f1af55df2b4d71f05ee94579c4c31a303f29015
SarahHa <88617817+SarahHaKG@users.noreply.github.com>
2025-03-16 11:57:58 +0100

    output file

--- prompt_generation/prompt_generation__few_shot_negation_query.py
+++ prompt_generation/prompt_generation__few_shot_negation_query.py
@@ -3,7 +3,7 @@ import os
 # This script is to generate, zero-shot, few-shot, chain of thoughts prompts
 # First run : python prompt_generation_negation_query.py ../data_optimized/1c_data_final_${dataset}_test_2.csv --context_length 1000 --nrows $nrows
 # Then use the previous output file as input arg to this script, for example in this case the input file will be called 1c_prompt_${dataset}_test_2.csv
-# And the generated output file name will be 1c_prompt_${dataset}_test_2_CoT.csv 
+# And the generated output file name will be called 1c_prompt_${dataset}_test_2_CoT.csv 
 
 # Set up argument parsing
 parser.add_argument("file_path", type=str, help="Path to the input file in csv format")
@@ -126,5 +126,4 @@ df['prompt_negation_query']=df['prompt_negation_query'].apply(lambda row: row.re
 base_name = os.path.basename(file_path)  
 input_file_name, ext = os.path.splitext(base_name)  
 output_file_name = f"{input_file_name}{suffix}{ext}"  
-
 df.to_csv(output_file_name)
